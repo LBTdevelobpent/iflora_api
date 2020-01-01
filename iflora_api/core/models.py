795e881c0e32f1a1eac4e54e829f7448f7985690
lucaslima18 <lucas.ala1999@gmail.com>
2020-01-01 11:54:17 -0300

    updating class diagram

--- iflora_api/core/models.py
+++ iflora_api/core/models.py
@@ -4,6 +4,17 @@ class BaseModel(models.Model):
     created_at= models.DateTimeField(auto_now_add=True)
     updated_at= models.DateTimeField(auto_now=True)
 
+class User(BaseModel):
+    name= 
+    last_name=
+    nickname=
+    registration= 
+    email=
+    password= 
+    type_user=
+    interest=
+
+
 
 
 
